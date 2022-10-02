013f6d26df1b420eefe07774beec9a2002a45edb
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-02 11:08:06 +0100

    added steps of creating router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+    { path: '/', element: <div>This is Default page</div> },
+    { path: '/home', element: <div>This is home page</div> },
+    { path: '/about', element: <div>This is about us page</div> }
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/steps.js
@@ -0,0 +1,6 @@
+/* 
+1. install react router : npm i react-router-dom
+2. create a router using createBrowserRouter
+3. Add RouterProvider and pass router props
+4. Create some route. this should be an object(2 things needed to create route path and element) elements could be any html tag or any component
+*/
