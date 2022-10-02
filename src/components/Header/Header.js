d815fe4cc357cccb533206b24b91a2dad1387617
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-10-02 16:47:32 +0100

    created active route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
         <div className='header'>
             <nav>
-                <Link to='/home'>Home</Link>
+                {/* create active link */}
+
+                <NavLink className={({ isActive }) => isActive ? 'active' : undefined} to='/home'>Home</NavLink>
+                <NavLink to='/about'>About</NavLink>
+                <NavLink to='/products'>Products</NavLink>
+                <NavLink to='/news'>News</NavLink>
+
+                {/* <Link to='/home'>Home</Link>
                 <Link to='/about'>About</Link>
                 <Link to='/products'>Products</Link>
-                <Link to='/news'>News</Link>
+                <Link to='/news'>News</Link> */}
             </nav>
 
             <h4>Common Header</h4>
